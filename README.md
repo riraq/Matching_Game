c2729d1034ca30d305c8319026323292b977eae9
riraq <riraq@mail.usf.edu>
2021-07-09 15:48:48 -0500

    adding started files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Matching_Game
 Simple Matching Game
+
+Based on https://www.youtube.com/watch?v=XD5sZWxwJUk
---
+++ app.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  const width = 8;
+  const squares = [];
+
+  const candyColors = [
+    'red',
+    'yellow',
+    'orange',
+    'purple',
+    'green',
+    'blue'
+  ]
+
+  // Create Board
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('draggable', true)
+      let randomColor = Math.floor(Math.random() * candyColors.length)
+      square.style.backgroundColor = candyColors[randomColor]
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  };
+  createBoard();
+
+
+
+
+});
