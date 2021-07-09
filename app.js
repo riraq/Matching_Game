08532ac2a7d6f59dc25ed73623781173b055b387
riraq <riraq@mail.usf.edu>
2021-07-09 16:14:46 -0500

    squares switch spots with adjacent squares when dragged over

--- app.js
+++ app.js
@@ -32,39 +32,58 @@ document.addEventListener('DOMContentLoaded', () => {
   let squareIdBeingDragged;
   let squareIdBeingReplaced;
 
-  squares.forEach(square => square.addEventListener('dragstart', dragStart))
-  squares.forEach(square => square.addEventListener('dragend', dragEnd))
-  squares.forEach(square => square.addEventListener('dragover', dragOver))
-  squares.forEach(square => square.addEventListener('dragenter', dragEnter))
-  squares.forEach(square => square.addEventListener('dragleave', dragLeave))
-  squares.forEach(square => square.addEventListener('drop', dragDrop))
+  squares.forEach(square => square.addEventListener('dragstart', dragStart));
+  squares.forEach(square => square.addEventListener('dragend', dragEnd));
+  squares.forEach(square => square.addEventListener('dragover', dragOver));
+  squares.forEach(square => square.addEventListener('dragenter', dragEnter));
+  squares.forEach(square => square.addEventListener('dragleave', dragLeave));
+  squares.forEach(square => square.addEventListener('drop', dragDrop));
 
   function dragStart() {
-    colorBeingDragged = this.style.backgroundColor
-    squareIdBeingDragged = parseInt(this.id)
-  }
+    colorBeingDragged = this.style.backgroundColor;
+    squareIdBeingDragged = parseInt(this.id);
+  };
 
   function dragEnd() {
+    // defining valid moves
+    let validMoves = [
+      squareIdBeingDragged -1, 
+      squareIdBeingDragged -width,
+      squareIdBeingDragged +1,
+      squareIdBeingDragged +width 
+    ];
 
-  }
+    let validMove = validMoves.includes(squareIdBeingReplaced);
+
+    if(squareIdBeingReplaced && validMove) {
+      squareIdBeingReplaced = null;
+    } else if(squareIdBeingReplaced && !validMove){
+      squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced;
+      squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged;
+    } else{
+      squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged;
+    };
+
+  };
 
   function dragOver(e) {
     e.preventDefault()
-  }
+  };
 
   function dragEnter() {
 
-  }
+  };
 
   function dragLeave() {
 
-  }
+  };
 
   function dragDrop() {
-    colorBeingReplaced = this.style.backgroundColor
-    squareIdBeingReplaced = parseInt(this.id)
-    squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced
-  }
+    colorBeingReplaced = this.style.backgroundColor;
+    squareIdBeingReplaced = parseInt(this.id);
+    this.style.backgroundColor = colorBeingDragged;
+    squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced;
+  };
 
 
 });
